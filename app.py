d1e73a2da36f1fe7c90aa2c05853d44519ffa61a
Sahilrathod3737 <sr982637@gmail.com>
2026-04-22 09:33:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -316,6 +316,5 @@ def download_report():
         return f"Layout Error: {str(e)}"
 
 if __name__ == '__main__':
-    # Cloud platform se port lena, nahi toh default 5000 use karna
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port)
